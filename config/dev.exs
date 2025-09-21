6901bb91ed21164f62720462e24ef7f529058eb9
Zach Daniel <zach@zachdaniel.dev>
2025-09-20 22:45:07 -0400

    chore: add git_ops config

--- config/dev.exs
+++ config/dev.exs
@@ -3,6 +3,8 @@ import Config
 config :git_ops,
   mix_project: Mix.Project.get!(),
   types: [types: [tidbit: [hidden?: true], important: [header: "Important Changes"]]],
+  repository_url: "https://github.com/ash-project/ash_events",
+  github_handle_lookup?: true,
   version_tag_prefix: "v",
   manage_mix_version?: true,
   manage_readme_version: true

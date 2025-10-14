aefa26c0d68eac2eb7920d9d4a8bd61446f9847c
James Harton <james@harton.nz>
2025-10-15 09:03:37 +1300

    chore: Fix REUSE copyright attributions

--- test/support/accounts/domain.ex
+++ test/support/accounts/domain.ex
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024 Torkild G. Kjevik
+# SPDX-FileCopyrightText: 2023 ash_events contributors <https://github.com/ash-project/ash_events/graphs.contributors>
 #
 # SPDX-License-Identifier: MIT
 
